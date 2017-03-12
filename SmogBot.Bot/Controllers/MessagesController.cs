0d956a59c5c86631d2b6c8ee16b3a043f50dddc6
luktom <luktom@luktom.net>
2017-03-12 16:33:00 +0100

    Add general exception catch in controller

--- SmogBot.Bot/Controllers/MessagesController.cs
+++ SmogBot.Bot/Controllers/MessagesController.cs
@@ -1,8 +1,10 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Bot.Connector;
+using Tomaszkiewicz.BotFramework.Extensions;
 
 namespace SmogBot.Bot.Controllers
 {
@@ -18,34 +20,43 @@ namespace SmogBot.Bot.Controllers
 
         public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
         {
-            switch (activity.GetActivityType())
+            try
             {
-                case ActivityTypes.Message:
-                    await _bot.OnMessage(activity);
-                    break;
+                switch (activity.GetActivityType())
+                {
+                    case ActivityTypes.Message:
+                        await _bot.OnMessage(activity);
+                        break;
 
-                case ActivityTypes.ConversationUpdate:
-                    await _bot.OnConversationUpdate(activity);
-                    break;
+                    case ActivityTypes.ConversationUpdate:
+                        await _bot.OnConversationUpdate(activity);
+                        break;
 
-                case ActivityTypes.Trigger:
-                    await _bot.OnConversationUpdate(activity);
-                    break;
+                    case ActivityTypes.Trigger:
+                        await _bot.OnConversationUpdate(activity);
+                        break;
 
-                case ActivityTypes.ContactRelationUpdate:
-                    await _bot.OnContactRelationUpdate(activity);
-                    break;
+                    case ActivityTypes.ContactRelationUpdate:
+                        await _bot.OnContactRelationUpdate(activity);
+                        break;
 
 
-                case ActivityTypes.DeleteUserData:
-                    await _bot.OnDeleteUserData(activity);
-                    break;
+                    case ActivityTypes.DeleteUserData:
+                        await _bot.OnDeleteUserData(activity);
+                        break;
 
-                //case ActivityTypes.Typing:
-                //case ActivityTypes.Ping:
-                default:
-                    await _bot.OnUnknownActivity(activity);
-                    break;
+                    //case ActivityTypes.Typing:
+                    //case ActivityTypes.Ping:
+                    default:
+                        await _bot.OnUnknownActivity(activity);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                var connector = activity.CreateConnectorClient();
+
+                connector.Conversations.SendToConversation(activity.CreateReply("Controller exception: " + ex.Message + ex.StackTrace));
             }
 
             return Request.CreateResponse(HttpStatusCode.OK);
