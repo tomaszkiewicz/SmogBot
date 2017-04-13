85c760b0d17d67f67598f9f854c98cfb0261e3de
luktom <luktom@luktom.net>
2017-04-13 11:58:23 +0200

    Add missing files

---
+++ SmogBot.Database/Bot/Views/WarningsStatus.sql
@@ -0,0 +1,13 @@
+﻿CREATE VIEW [Bot].[WarningsStatus] AS
+SELECT
+    u.[ConversationId], COUNT([lw].[UserId]) AS WarningsEnabled
+FROM
+    [dbo].[Users] [u]
+LEFT JOIN (
+           SELECT DISTINCT
+            [UserId]
+           FROM
+            [dbo].[LastWarnings]
+          ) [lw]
+ON  [lw].[UserId] = [u].[Id]
+GROUP BY [u].[ConversationId]
