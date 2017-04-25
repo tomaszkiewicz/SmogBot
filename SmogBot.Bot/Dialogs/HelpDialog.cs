04fd5132ac3cdeab7a9b482c10a4740e53352b82
luktom <luktom@luktom.net>
2017-04-25 15:21:19 +0200

    Fix encoding, again...

--- SmogBot.Bot/Dialogs/HelpDialog.cs
+++ SmogBot.Bot/Dialogs/HelpDialog.cs
@@ -11,24 +11,24 @@ namespace SmogBot.Bot.Dialogs
     {
         public async Task StartAsync(IDialogContext context)
         {
-            await context.PostAsync("Moim zadaniem jest pomaganie Ci przetrwaæ w zasmogowanych polskich miastach :)");
+            await context.PostAsync("Moim zadaniem jest pomaganie Ci przetrwaÄ‡ w zasmogowanych polskich miastach :)");
             await context.SendTypingMessage();
             await Task.Delay(3000);
-            await context.PostAsync("Do tego celu jestem wyposa¿ony w kilka narzêdzi:");
+            await context.PostAsync("Do tego celu jestem wyposaÅ¼ony w kilka narzÄ™dzi:");
             await context.SendTypingMessage();
             await Task.Delay(3000);
-            await context.PostAsync("Mogê na ¿¹danie sprawdziæ poziom zanieczyszczeñ w danym mieœcie - wystarczy, ¿e klikniesz opcjê 'SprawdŸ przekroczenia norm' w menu g³ównym.");
+            await context.PostAsync("MogÄ™ na Å¼Ä…danie sprawdziÄ‡ poziom zanieczyszczeÅ„ w danym mieÅ›cie - wystarczy, Å¼e klikniesz opcjÄ™ 'SprawdÅº przekroczenia norm' w menu gÅ‚Ã³wnym.");
             await Task.Delay(5000);
             await context.SendTypingMessage();
             await Task.Delay(4000);
-            await context.PostAsync("Jeœli nie masz czasu na regularne klikanie - nie ma sprawy, mogê dzia³¹æ tak¿e pasywnie, powiadamiaj¹c Ciê o przekroczeniach, gdy tylko wyst¹pi¹, jak równie¿ o zadanych przez Ciebie godzinach :)");
+            await context.PostAsync("JeÅ›li nie masz czasu na regularne klikanie - nie ma sprawy, mogÄ™ dziaÅ‚Ä…Ä‡ takÅ¼e pasywnie, powiadamiajÄ…c CiÄ™ o przekroczeniach, gdy tylko wystÄ…piÄ…, jak rÃ³wnieÅ¼ o zadanych przez Ciebie godzinach :)");
             await Task.Delay(5000);
             await context.SendTypingMessage();
             await Task.Delay(4000);
-            await context.PostAsync("Ustawisz to klikaj¹c opcjê 'Powiadomienia i ostrze¿enia' w menu g³ównym.");
+            await context.PostAsync("Ustawisz to klikajÄ…c opcjÄ™ 'Powiadomienia i ostrzeÅ¼enia' w menu gÅ‚Ã³wnym.");
             await Task.Delay(3000);
             
-            await context.PostAsync($"[A moja wersja to: {Assembly.GetExecutingAssembly().GetName().Version} - to tak dla u³atwienia diagnostyki :)]");
+            await context.PostAsync($"[A moja wersja to: {Assembly.GetExecutingAssembly().GetName().Version} - to tak dla uÅ‚atwienia diagnostyki :)]");
 
             context.Done(new object());
         }
